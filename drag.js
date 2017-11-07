c81721deb5dc420a54ad67931fe01df7dd46c1f1
thsth <thsth666@163.com>
2017-11-07 14:00:50 +0800

    change3 drag.js and demo1.html

--- drag.js
+++ drag.js
@@ -2,7 +2,7 @@ function drag(id){
     var obj = document.getElementById(id);
     var pageX = 0;
     var pageY = 0;
-    var pageZ = 0;
+    var pageW = 0;
     obj.onmousedown = function(){
     }
 }
